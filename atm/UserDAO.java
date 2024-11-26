ca9e92c07c2ea485d4c7f7a38c1a93b4efc51d3f
Sai Naga Siddhartha Babu Kocharlakota <113345981+siddharthababu@users.noreply.github.com>
2024-11-26 18:22:29 +0530

    Add files via upload

---
+++ atm/UserDAO.java
@@ -0,0 +1,14 @@
+package com.codegnan.dao;
+import java.util.List;
+
+import com.codegnan.entity.User;
+ 
+public interface UserDAO {
+	void addUser(User user);
+	User getUserById(int id);
+	List<User> getAllUsers();
+	void updateUser(User user);
+	void deleteUser(int id);
+}
+ 
+ 
